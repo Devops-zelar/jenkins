3a8762f1d7222b43d7f8fc9ee53b6f693ccd4be0
eshwar <ramanieshwar091@gmail.com>
2021-05-27 16:40:22 +0530

    new commit

--- seed.groovy
+++ seed.groovy
@@ -3,38 +3,25 @@ folder('CI-Pipelines') {
   description('CI Pipelines')
 }
 
-def component = ["cart", "catalogue","user","payment","shipping","frontend"];
 
-def count=(component.size()-1)
-for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("CI-Pipelines/${j}-ci") {
-    configure { flowdefinition ->
-      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-        'triggers' {
-          'hudson.triggers.SCMTrigger' {
-            'spec'('*/2 * * * 1-5')
-            'ignorePostCommitHooks'(false)
+
+pipelineJob('frontend') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+      'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/Devops-zelar/frontend.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
           }
         }
       }
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/zsdevops01/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(falsein)
     }
   }
 }
