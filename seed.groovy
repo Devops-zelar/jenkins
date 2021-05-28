a84e70af8ab8f9c702c1fc779cab997c9e920b77
eshwar <ramanieshwar091@gmail.com>
2021-05-28 11:47:14 +0530

    users

--- seed.groovy
+++ seed.groovy
@@ -45,3 +45,26 @@ pipelineJob('login') {
   }
 }
 
+
+
+pipelineJob('users') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+      'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/Devops-zelar/users.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
+
