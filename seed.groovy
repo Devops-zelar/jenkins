cfdf7490c0b53d9ee671c071fb2c038d01525f83
eshwar <ramanieshwar091@gmail.com>
2021-05-28 11:51:19 +0530

    todo

--- seed.groovy
+++ seed.groovy
@@ -66,5 +66,29 @@ pipelineJob('users') {
       'lightweight'(true)
     }
   }
-}
+
+
+
+
+
+  pipelineJob('todo') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/Devops-zelar/todo.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
 
